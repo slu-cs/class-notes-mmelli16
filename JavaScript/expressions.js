945575db8d682a0568d3f614d9b2dcecbb371141
Matthew Ellis <mmelli16@stlawu.edu>
2020-01-29 13:36:58 -0500

    JS intro

---
+++ JavaScript/expressions.js
@@ -0,0 +1,54 @@
+//Expression syntax in JavaScript
+
+//Bools
+true
+false
+
+//logic
+!false
+true && false
+true || false
+
+//numbers are all 1 type (no int vs double vs float)
+3.14
+
+//Equality
+3 === 3.0
+3 !== 3.14
+
+//Ordering
+3 > 2
+3 >= 2
+2 < 3
+2 <= 3
+
+//String
+'hello'
+
+//Concatenation
+
+'a' + 'b' = 'ab'
+
+//Arithmatic
+3 + 2 === 5
+3 - 2 === 1
+3 * 2 === 6
+3 / 2 === 1.5
+3 % 2 === 1
+3 ** 2 === 9
+
+//Type Coercion
+'3' + 2 === '32'
+'3' + true === '3true'
+'3'- 2 === 1
+3 + true === 4
+
+//Loose Equality
+'3' == 3
+true == 1
+false == 0
+false == ''
+'' == 0
+
+//template strings
+`3 + 2 is ${3+2}`
