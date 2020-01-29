c5f3c9901c9f8d8caaf5c95ea29ed8546a4e4920
Matthew Ellis <mmelli16@stlawu.edu>
2020-01-29 14:06:39 -0500

    practice2

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -5,3 +5,15 @@ while (count <= 10){
   count += 1
 }
 console.log(total);
+
+let fib1=1;
+let fib2=1;
+let fibtotal=0;
+let temp1=0
+while fibtotal<1000{
+  fibtotal=temp1;
+  fibtotal=fib1+fib2;
+  fib1=fib2;
+  fib2=temp1;
+}
+console.log(fibtotal);
