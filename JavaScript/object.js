67105aa0560648e6e8ed9d1cee30c6db793e54fc
Matthew Ellis <mmelli16@stlawu.edu>
2020-02-03 13:24:17 -0500

    functions

--- JavaScript/object.js
+++ JavaScript/object.js
@@ -40,3 +40,24 @@ languages.push('MongoDB');
 for (const lang of languages){
   console.log(lang);
 }
+
+//function syntax
+
+//function definition
+const avg= function(a,b) {
+  return (a+b) / 2;
+};
+
+//function call
+console.log(avg(5, 6));
+
+//Another definition
+const print = function(a, b) {
+  console.log(` a=${a}, b=${b}`);
+}
+
+//undefined values
+const result = print(7,8);
+console.log(result);
+print(9);
+print();
