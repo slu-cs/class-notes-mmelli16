e51aaaf6b33719ac4c67dd1baf7f78e8c29b0422
Matthew Ellis <mmelli16@stlawu.edu>
2020-02-03 13:07:08 -0500

    circle object

--- JavaScript/object.js
+++ JavaScript/object.js
@@ -3,3 +3,20 @@
 //object literal
 const point = {x:1, y:2};
 console.log(point);
+
+console.log(point.x);
+
+//change object properties
+point.x = 3;
+
+//nested object literal
+const circle = {
+    center: point,
+    radius: 4
+};
+
+//access subproperties
+console.log(circle.center.x);
+console.log(circle.color);
+circle.color = 'blue';
+console.log(circle);
